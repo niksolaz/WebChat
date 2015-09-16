910c0ebe16083f559f94c6e12dbd822a72e3cee2
niksolaz <solazzo.nicola@gmail.com>
2015-09-16 16:43:47 +0200

    hello entry

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3001;
+
+app.get('/',function(req,res){
+	res.send('Hi by Web Chat!');
+});
+
+app.listen(port);
+console.log('Web chat app in connected: ' + port);
