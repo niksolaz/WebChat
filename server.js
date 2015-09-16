efcd8023ba7f121825c86f592931fb30cbe04bf9
niksolaz <solazzo.nicola@gmail.com>
2015-09-16 19:43:48 +0200

    socket.io module

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3001;
 
 app.get('/',function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile('/index.html');
 });
 
+io.on('connection',function(socket){
+	console.log('a user connected');
+});
 app.listen(port);
 console.log('Web chat app in connected: ' + port);
 
